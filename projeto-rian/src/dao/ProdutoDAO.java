c174f049fe33217fd0302e33e7c700800cf8021c
RianFriedrich <125458446+RianFriedrich@users.noreply.github.com>
2023-11-30 16:47:22 -0300

    Add files via upload

---
+++ projeto-rian/src/dao/ProdutoDAO.java
@@ -0,0 +1,15 @@
+package dao;
+
+import conexao.Conexao;
+import java.sql.Connection;
+
+public class ProdutoDAO {
+    private Connection conn;
+    private Conexao conexao;
+    
+    public ProdutoDAO() {
+        this.conexao = new Conexao();
+        this.conn = conexao.getConexao();
+    }
+    
+}
